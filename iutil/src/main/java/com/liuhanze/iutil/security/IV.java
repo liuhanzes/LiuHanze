24b7ec85688227226f256f2b9bc4fcc81374a817
yuxuehandong <liuhanze@126.com>
2024-09-06 10:45:47 +0800

    add IV

---
+++ iutil/src/main/java/com/liuhanze/iutil/security/IV.java
@@ -0,0 +1,14 @@
+package com.liuhanze.iutil.security;
+
+/**
+ * @author: LiuHanze
+ * @date: 2024/9/6
+ */
+public class IV {
+
+    public static String S8 = "0000000000000000";
+    public static String S16 = "00000000000000000000000000000000";
+    public static byte[] B8 = new byte[]{0X00,0X00,0X00,0X00,0X00,0X00,0X00,0X00};
+    public static byte[] B16 = new byte[]{0X00,0X00,0X00,0X00,0X00,0X00,0X00,0X00,0X00,0X00,0X00,0X00,0X00,0X00,0X00,0X00};
+
+}
